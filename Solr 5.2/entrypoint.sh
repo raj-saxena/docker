723f8416c41bb3d6946d5df3120606c66ea3100d
Raj Saxena <raj-saxena@users.noreply.github.com>
2015-06-18 18:27:42 +0530

    Solr with supervisor

--- Solr 5.2/entrypoint.sh
+++ Solr 5.2/entrypoint.sh
@@ -4,14 +4,11 @@ set -e
 echo "Passed arguments-->\"$@\""
 
 if [ -z "$1" ]; then
-	echo "Usage: run <image> start|stop|<COMMAND> "
+	echo "Usage: run <image> start|<COMMAND> "
 elif [ "$1" == 'start' ]; then
 	echo "===== starting Solr ==== "
-    /solr/bin/solr start -f
-	#/usr/bin/supervisord
-elif [ "$1" == 'stop' ]; then
-	echo "===== stopping Solr ==== "
-	/solr/bin/solr stop -p 8989
+    #/solr/bin/solr start -f
+	/usr/bin/supervisord
 else 
 	exec "$@"
 fi
